203034c9c346257dd8cde17b078b7f5c92c1c88c
TyVik <tyvik8@gmail.com>
2014-05-19 21:25:38 +0400

    Change deprecated direct_to_template to TemplateView. Add namespace for app 'items'.

--- markettyvik/urls.py
+++ markettyvik/urls.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 #from django.contrib import admin
 #admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^items/', include('items.urls')),
-    url(r'^$', direct_to_template, {'template': 'index.html'})
+    url(r'^items/', include('items.urls', namespace="items")),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 #    url(r'^admin/', include(admin.site.urls)),
     # Examples:
     # url(r'^$', 'markettyvik.views.home', name='home'),
