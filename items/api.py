9417b8d4ec0e9f3854a688d1bfbd01feab45c9a2
TyVik <tyvik8@gmail.com>
2014-05-19 22:54:02 +0400

    Add API for Items.

--- items/api.py
+++ items/api.py
@@ -1,39 +1,16 @@
 ﻿# coding: utf-8
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from tastypie.authentication import Authentication
 
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-import django
 from models import Items
 
-def list(request):
-    items = Items.objects.values('id', 'name')
-    result = ''
-    for item in items:
-        result += str(item['id']) + '^' + item['name'] + '$'
-    return HttpResponse(result[:-1])
 
-def delete(request, id):
-    Items.objects.filter(id=id).delete()
-    return HttpResponse('')
-
-@csrf_exempt
-def add(request):
-    result = HttpResponse(400)
-    if request.method == 'POST':
-        print request.POST
-        item = Items()
-        item.name = request.POST['name']
-        item.save()
-        result = HttpResponse('')
-    return result
-
-def help(request):
-    help = """
-<h1>API для доступа к списку покупок</h1>
-<ul>
-<li>list - получить список продуктов</li>
-<li>delete - удалить продукт</li>
-<li>add - добавить продукт</li>
-<ul>
-"""
-    return HttpResponse(help)
+class ItemsResource(ModelResource):
+    class Meta:
+        queryset = Items.objects.all()
+        resource_name = 'items'
+        allowed_methods = ['post', 'get', 'patch', 'delete']
+        authentication = Authentication()
+        authorization = Authorization()
+        always_return_data = True
--- markettyvik/urls.py
+++ markettyvik/urls.py
@@ -2,22 +2,15 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import TemplateView
+from tastypie.api import Api
+from items.api import ItemsResource
 
-# Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+
+v1_api = Api(api_name='v1')
+v1_api.register(ItemsResource())
 
 urlpatterns = patterns('',
     url(r'^items/', include('items.urls', namespace="items")),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-#    url(r'^admin/', include(admin.site.urls)),
-    # Examples:
-    # url(r'^$', 'markettyvik.views.home', name='home'),
-    # url(r'^markettyvik/', include('markettyvik.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(v1_api.urls)),
 )
