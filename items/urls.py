3cf050683157b1f006584f86724cff94c2843ffa
TyVik <tyvik8@gmail.com>
2014-05-19 21:30:04 +0400

    AdvancedListView change to FormAndListView. Update template items.html.

--- items/urls.py
+++ items/urls.py
@@ -1,15 +1,19 @@
 # coding: utf-8
 
-from django.conf.urls.defaults import patterns, url, include
-from views import ItemsView
-import api
+from django.conf.urls.defaults import patterns, url
+from views import FormAndListView, delete
+from models import Items
+from forms import ItemsForm
+from django.core.urlresolvers import reverse_lazy
+
 
 urlpatterns = patterns('',
-    url(r'^$', ItemsView.as_view()),
-    url(r'delete/(?P<pk>\d*)/$', ItemsView.as_delete()),
-
-    url(r'api/list$', api.list),
-    url(r'api/add$', api.add),
-    url(r'api/delete/(\d*)$', api.delete),
-    url(r'api/$', api.help)
+    url(r'^$', FormAndListView.as_view(
+        queryset=Items.objects.all(),
+        context_object_name='itemsProd',
+        form_class=ItemsForm,
+        success_url=reverse_lazy('items:index'),
+        template_name='items/items.html',
+    ), name="index"),
+    url(r'delete/(?P<pk>\d+)', delete)
 )
--- items/views.py
+++ items/views.py
@@ -1,13 +1,31 @@
 # coding: utf-8
 
 from models import Items
-from utils.advancedListView import AdvancedListView
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from decorators import ajax_required
+from django.views.generic import ListView
 
-class ItemsView(AdvancedListView):
-    model = Items
-    context_object_name = 'itemsProd'
-    template_name = 'items.html'
-    success_url = '/items/'
 
-    def get_queryset(self):
-        return Items.objects.all()
+class FormAndListView(ListView):
+
+    form_class = None
+    success_url = None
+
+    def get_context_data(self, **kwargs):
+        context = super(self.__class__, self).get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        return context
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+@ajax_required
+def delete(request, pk):
+    item = get_object_or_404(Items, pk=pk)
+    item.delete()
+    return HttpResponse("ok")
