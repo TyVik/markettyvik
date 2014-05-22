3c2ae1516a174fe04052d158fe4083f64d398610
Viktor Tyshchenko <tyvik8@gmail.com>
2014-05-22 12:17:43 +0400

    Add bootstrap3 support.

--- items/forms.py
+++ items/forms.py
@@ -4,5 +4,9 @@ from django.forms import ModelForm
 from models import Items
 
 class ItemsForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ItemsForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs["class"] = "input-sm"
+
     class Meta:
         model = Items
