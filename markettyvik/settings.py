a4d33aa74d6fa21c09d04722f92dbd719b146050
Viktor Tyshchenko <tyvik8@gmail.com>
2014-05-26 16:47:49 +0400

    Static for project moved to markettyvik directory.

--- markettyvik/settings.py
+++ markettyvik/settings.py
@@ -1,7 +1,9 @@
 # Django settings for markettyvik project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PROJECT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 
 ADMINS = (
     ('TyVik', 'tyvik8@gmail.com'),
@@ -59,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,7 +69,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    'static',
+    'markettyvik/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
